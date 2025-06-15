23b77217af88e2d689009405405c215b8896c5ce
Liubomyr Midzhak <lmidzhak@gmail.com>
2025-06-15 23:00:16 +0300

    Added packages for topics, posts and comments with models

---
+++ posts/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+
+from database import Base
+
+
+class DBPost(Base):
+    __tablename__ = 'post'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now
+    )
+    updated_at: Mapped[DateTime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now
+    )
+    topic_id: Mapped[int] = mapped_column(ForeignKey('topic.id'), nullable=False)
+
+    topic: Mapped["DBTopic"] = relationship(
+        'topics.models.DBTopic',
+        back_populates='posts'
+    )
+    comments: Mapped["DBComment"] = relationship(
+        'comments.models.DBComment',
+        back_populates='post',
+        cascade="all, delete-orphan"
+    )
