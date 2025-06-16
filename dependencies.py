fa6434ceadd71d2b8a145fb0372d5929d5f3a201
Liubomyr Midzhak <lmidzhak@gmail.com>
2025-06-17 01:48:18 +0300

    Added main, utils modules, edited dependencies module

--- dependencies.py
+++ dependencies.py
@@ -1,6 +1,18 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Header, HTTPException, status
 
 from database import SessionLocal
+
+
+TOKEN = "mytoken"
+
+
+async def verify_token(authorization: str = Header(...)):
+    if authorization != f"Bearer {TOKEN}":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing token",
+        )
 
 
 async def get_db() -> AsyncSession:
